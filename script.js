934e1ef5c84d8bdd6d322efa57d6dfcb6b6020db
starko20 <starc.jure@gmail.com>
2024-12-01 10:05:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("BetTrackerPro Loaded!");
 
-  // Poišči gumb z ID-jem 'click-me'
+  // Klik na gumb
   const button = document.getElementById("click-me");
-
-  // Preveri, ali obstaja gumb
-  if (button) {
-    button.addEventListener("click", () => {
-      alert("You clicked the button!");
-    });
-  } else {
-    console.error("Gumb z ID 'click-me' ni najden.");
-  }
+  button.addEventListener("click", () => {
+    alert("You clicked the button!");
+  });
 });
